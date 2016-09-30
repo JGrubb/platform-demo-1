1c851ea837fde47a4a805314714d6692b3704e83
John Grubb <grubb@platform.sh>
2016-09-30 17:49:28 -0400

    one more time

--- public/config.php
+++ public/config.php
@@ -1,19 +1,20 @@
 <?php
 
-require 'vendor/autoload.php';
+require __DIR__ .'/../vendor/autoload.php';
 
 $config = new \Platformsh\ConfigReader\Config();
 
 // You can check for any particular value being available (recommended):
-if (isset($config->relationships['database'][0])) {
-    $database = $config->relationships['database'][0];
 
-    // Now $database is an array representing a database service.
-}
 
 // Or you can check that any configuration is available at all:
 if ($config->isAvailable()) {
     var_dump($config->project);
     var_dump($config->application_name);
     var_dump($config->relationships['database'][0]['host']);
+    if (isset($config->relationships['database'][0])) {
+        $database = $config->relationships['database'][0];
+
+        // Now $database is an array representing a database service.
+    }
 }
