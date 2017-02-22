6f4ebda3b8015abaa23fb5f54c2d2d1500fb9096
John Grubb <grubb@johnnygrubb.com>
2017-02-22 14:34:02 -0500

    moving Pure CSS CDN to cdnjs

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
     <title>Platform.sh Example Demo</title>
 
 
-<link rel="stylesheet" href="//yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.6.2/pure-min.css">
 
 
 
@@ -19,7 +19,7 @@
 <![endif]-->
 <!--[if gt IE 8]><!-->
   
-    <link rel="stylesheet" href="//yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.6.2/grids-responsive-min.css">
   
 <!--<![endif]-->
 
