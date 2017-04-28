cf68a0c435f9f41843c5f288970a15054922cb6c
John Grubb <grubb@johnnygrubb.com>
2017-04-28 15:22:35 -0400

    adding gzip to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
+var gzip = require('gulp-gzip');
 
 gulp.task('default', function() {
   return gulp.src('./assets/sass/*.sass')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gzip())
     .pipe(gulp.dest('./public/css'))
 });
 
