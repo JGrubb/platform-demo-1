655da53d83629b32ccb53ec6942dbf415934a151
John Grubb <grubb@platform.sh>
2019-08-05 10:35:11 -0400

    adding filebeat

---
+++ config/filebeat/scripts/install_filebeat.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+TEMP_BEAT_HOME=config/filebeat/build
+
+[ ! -d $TEMP_BEAT_HOME ] && mkdir -p $TEMP_BEAT_HOME
+cd $TEMP_BEAT_HOME
+
+# Install Filebeat
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.0-linux-x86_64.tar.gz
+tar xzvf filebeat-7.3.0-linux-x86_64.tar.gz
+rm filebeat-7.3.0-linux-x86_64.tar.gz
+
+mkdir -p filebeat-7.3.0-linux-x86_64/pki/tls/certs
