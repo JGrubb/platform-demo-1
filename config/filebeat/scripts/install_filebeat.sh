295ae8b19b9528704022e8ed55fb87c87818d389
John Grubb <grubb@platform.sh>
2019-08-05 11:20:02 -0400

    make variable

--- config/filebeat/scripts/install_filebeat.sh
+++ config/filebeat/scripts/install_filebeat.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 TEMP_BEAT_HOME=config/filebeat/build
+VERSION="7.3.0"
 
 [ ! -d $TEMP_BEAT_HOME ] && mkdir -p $TEMP_BEAT_HOME
 cd $TEMP_BEAT_HOME
 
 # Install Filebeat
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.0-linux-x86_64.tar.gz
-tar xzvf filebeat-7.3.0-linux-x86_64.tar.gz
-rm filebeat-7.3.0-linux-x86_64.tar.gz
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$VERSION-linux-x86_64.tar.gz
+tar xzvf filebeat-$VERSION-linux-x86_64.tar.gz
+rm filebeat-$VERSION-linux-x86_64.tar.gz
 
-mkdir -p filebeat-7.3.0-linux-x86_64/pki/tls/certs
+mkdir -p filebeat-$VERSION-linux-x86_64/pki/tls/certs
 
-mv $HOME/filebeat.yml filebeat-7.3.0-linux-x86_64/
+mv $HOME/filebeat.yml filebeat-$VERSION-linux-x86_64/
 
-cp -v -r $HOME/config/filebeat/build/filebeat-6.7.0-linux-x86_64/* $HOME/filebeat
+cp -v -r $HOME/config/filebeat/build/filebeat-$VERSION-linux-x86_64/* $HOME/filebeat
